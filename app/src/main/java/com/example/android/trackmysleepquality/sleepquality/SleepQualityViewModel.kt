a5683d54a72399f66c28d48fcc07078cdbbea1fc
jrupin <jean.rupin@gmail.com>
2020-10-05 21:37:06 +0200

    Add Creation Database Singleton

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
@@ -15,3 +15,38 @@
  */
 
 package com.example.android.trackmysleepquality.sleepquality
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    abstract val sleepDatabaseDao : SleepDatabaseDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE : SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this){
+                var instance = INSTANCE
+
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    ).fallbackToDestructiveMigration().build()
+                }
+
+                return instance
+            }
+
+        }
+    }
+}
